201c6bbca85e5425545ad93388118de9930b8265
matheusnascimentoti99 <matheusnascimentoti99@gmail.com>
2022-09-27 00:34:58 -0300

    :hammer: Change dockerfile

--- karaf/init.sh
+++ karaf/init.sh
@@ -2,4 +2,7 @@
 $KARAF_HOME/bin/start
 sleep 10
 client -r 10 -f /karaf.conf
+
+# sed '/^<transportConnector.* /a <transportConnector name="mqtt" uri="mqtt://0.0.0.0:1883"/>' $KARAF_HOME/etc/activemq.xml
+
 client
