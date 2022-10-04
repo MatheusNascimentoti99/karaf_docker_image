19d14f1226ecdc799ab7b219815d9d37df2c7b70
matheusnascimentoti99 <matheusnascimentoti99@gmail.com>
2022-10-04 01:58:35 -0300

    :hammer: Manage up karaf

--- karaf/init.sh
+++ karaf/init.sh
@@ -3,4 +3,12 @@ $KARAF_HOME/bin/start
 sleep 10
 client -r 10 -f /karaf.conf
 
+sed '/^<transportConnector.*/i <transportConnector name="mqtt" uri="mqtt://0.0.0.0:1883"/>' $KARAF_HOME/etc/activemq.xml
+$KARAF_HOME/bin/stop
+sleep 1
+$KARAF_HOME/bin/start
+client -r 10 "bundle:install -s mvn:com.github.larsid/soft-iot-mapping-devices/master"
+client -r 10 "bundle:install -s mvn:br.ufba.dcc.wiser.soft_iot/soft-iot-local-storage/1.0.0"
+client -r 10 "bundle:install -s mvn:br.ufba.dcc.wiser.soft_iot/soft-iot-iot-service/1.0.0"
+
 client
